6189fe8d20d6b3240f5d42b1d4de82341552c467
benwittbrodt <benwittbrodt@gmail.com>
2020-05-17 14:25:21 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,12 +3,12 @@
 function benwittbrodt_files() {
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
   wp_enqueue_script('webflow-js', get_theme_file_uri('/js/webflow.js'), NULL, '1.0', true);
-  wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.5.1.slim.min.js');
-  wp_enqueue_script('popper-js', '//cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js');
-  wp_enqueue_script('bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js');
+  //wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.5.1.slim.min.js');
+  //wp_enqueue_script('popper-js', '//cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js');
+  //wp_enqueue_script('bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js');
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css');
-  wp_enqueue_style( 'bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css' );
+  //wp_enqueue_style( 'bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css' );
   wp_enqueue_style('benwittbrodt_main_styles', get_stylesheet_uri());
 }
 
@@ -22,8 +22,3 @@ function benwittbrodt_features() {
 }
 
 add_action('after_setup_theme', 'benwittbrodt_features');
-
-function jvw_post_types() {
-  
-}
-add_action('init','jvw_post_types');
