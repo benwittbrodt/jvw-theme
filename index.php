0b219df27504b1a7e3e74147367713e4d4f7d73f
benwittbrodt <benwittbrodt@gmail.com>
2020-05-17 08:39:37 -0400

    Create sections of website

--- index.php
+++ index.php
@@ -1,15 +1,37 @@
 <?php get_header(); ?>
 
-<div class="w-container">
-    <h2 id="resume">Resume</h2>
-    <img src="<?php echo get_theme_file_uri('/images/jillianresume2020.jpg');?>" alt="">
+<div class="w-container" id="resume">
+    <h2>Resume</h2>
+    <img src="<?php echo get_theme_file_uri('/images/resume2020.pdf');?>" width="100%" alt="">
 </div>
 
-<div class="w-container">
-    <h2 id="reels"> Reels</h2>
+<div class="w-container" id="reels">
+    <h2>Reels</h2>
+
+    <iframe width="100%" height="500" src="https://www.youtube.com/embed/plWSRaSv5qc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="100%" height="500" src="https://www.youtube.com/embed/r4yPKzAKDto" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
-<div class="w-container">
-    <h2 id="follow">Follow</h2>
+<div class="w-container" id="photos">
+    <h2>Photos</h2>
+<?php
+$photos = get_post( 24 );
+echo $photos->post_content;
+?>
+
+</div>
+
+<div class="w-container" id="contact">
+    <h2>Contact</h2>
+    <?php 
+        $contact = get_post( 28 );
+        if(have_posts($contact)) {
+            the_post();
+            the_content();
+        }
+        
+       
+    ?>
 </div>
 <?php get_footer(); ?>
