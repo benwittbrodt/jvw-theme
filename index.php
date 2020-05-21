5606106f77aacf2e607254ee61dd9380e2b92a07
benwittbrodt <benwittbrodt@gmail.com>
2020-05-21 15:50:04 -0400

    getting it set up

--- index.php
+++ index.php
@@ -1,36 +1,50 @@
 <?php get_header(); ?>
 
 <div class="w-container" id="resume">
-    <h2>Resume</h2>
+    <h1>Resume</h1>
+    <hr>
     <img src="<?php echo get_theme_file_uri('/images/resume2020.pdf');?>" width="100%" alt="">
 </div>
 
 <div class="w-container" id="reels">
-    <h2>Reels</h2>
-
-    <iframe width="100%" height="500" src="https://www.youtube.com/embed/plWSRaSv5qc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-    <iframe width="100%" height="500" src="https://www.youtube.com/embed/r4yPKzAKDto" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h1>Reels</h1>
+    <hr>
+    <br>
+    <?php
+        $post_id = 51;
+        $queried_post = get_post($post_id);
+        $content = $queried_post->post_content;
+        $content = apply_filters('the_content', $content);
+        $content = str_replace(']]>', ']]&gt;', $content);
+        echo $content;
+        wp_reset_postdata();
+    ?>
+    
 </div>
 
 <div class="w-container" id="photos">
-    <h2>Photos</h2>
-<?php
-$photos = get_post( 24 );
-echo $photos->post_content;
-?>
+    <h1>Photos</h1>
+    <hr>
+    <br>
+    <?php
+    $photos = get_post( 24 );
+    echo $photos->post_content;
+    ?>
 
 </div>
 
 <div class="w-container" id="contact">
-    <h2>Contact</h2>
+    <h1>Contact</h1>
+    <hr>
+    <br>
     <?php 
-        $contact = get_post( 28 );
-        if(have_posts($contact)) {
-            the_post();
-            the_content();
-        }
         
+        $post_id = 28;
+        $queried_post = get_post($post_id);
+        $content = $queried_post->post_content;
+        $content = apply_filters('the_content', $content);
+        $content = str_replace(']]>', ']]&gt;', $content);
+        echo $content;
        
     ?>
 </div>
